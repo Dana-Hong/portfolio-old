063a50ef0801cee6e8a820c05e4378e056ec810a
Dana <dana.hong@hotmail.com>
2022-11-18 11:13:30 -0500

    Add animations and meta description

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
-let width = document.querySelector(".logo");
-var onresize = function() {
-   //your code here
-   //this is just an example
-   width.innerText = document.body.clientWidth;
-}
-window.addEventListener("resize", onresize);
+// let width = document.querySelector(".logo");
+// var onresize = function() {
+//    //your code here
+//    //this is just an example
+//    width.innerText = document.body.clientWidth;
+// }
+// window.addEventListener("resize", onresize);
+const main = document.querySelector("main");
+
+const observerGenerator = (name) => {
+    return new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            entry.isIntersecting
+                ? entry.target.classList.add(`${name}-show`)
+                : entry.target.classList.remove(`${name}-show`);
+        });
+    });
+};
+
+const iconObserver = observerGenerator("icon");
+const textObserver = observerGenerator("text");
+
+const hiddenIcons = document.querySelectorAll(".icon-hidden");
+hiddenIcons.forEach((icon) => iconObserver.observe(icon));
+
+const hiddenText = document.querySelectorAll(".text-hidden");
+hiddenText.forEach((text) => textObserver.observe(text));
 
 document.querySelector(".btn").addEventListener("click", (event) => {
-   event.preventDefault();
-})
-
-document.querySelector('.nav__toggle').addEventListener('click', () => {
-   document.querySelector('header').classList.toggle('open-menu');
+    event.preventDefault();
 });
 
-Array.from(document.querySelectorAll('.nav__item')).forEach(navItem => navItem.addEventListener('click', () => {
-   document.querySelector('header').classList.toggle('open-menu');
-}));
+document.querySelector(".nav__toggle").addEventListener("click", () => {
+    document.querySelector("header").classList.toggle("open-menu");
+});
 
+window.Array.from(document.querySelectorAll(".nav__item")).forEach((navItem) =>
+    navItem.addEventListener("click", () => {
+        document.querySelector("header").classList.toggle("open-menu");
+    })
+);
+
+window.addEventListener("mousemove", (event) => {
+    console.log(event.clientX);
+    console.log(event.clientY);
+});
